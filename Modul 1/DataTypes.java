22a5bf7b880ddf5e32c832a6ef2ef04641b3ac8f
muhammadaditya85 <81172113+muhammadaditya85@users.noreply.github.com>
2023-01-15 18:13:37 +0800

    deklarasi variabel dan tipe data

---
+++ Modul 1/DataTypes.java
@@ -0,0 +1,13 @@
+public class DataTypes {
+ public static void main(String [] args) {
+
+  String namaDepan = "Mirza";
+  String namaTengah = "Yogi";
+  String namaBelakang = "Kurniawan";
+  int usia = 18;
+  int targetTahunKuliah = 4;
+  double ipk = 3.89764512;
+  char nilaiAbjad = 'A';
+  boolean tampan = true;
+ }
+}
