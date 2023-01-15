ef4c0a1cb5f7cfced7ce58f73ecd05debf4e07e8
muhammadaditya85 <81172113+muhammadaditya85@users.noreply.github.com>
2023-01-15 19:29:48 +0800

    Project IDEA

---
+++ Modul 1/Modul01_IDEA/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String [] args) {
+
+        String namaDepan = "Mirza";
+        String namaTengah = "Yogi";
+        String namaBelakang = "Kurniawan";
+        int usia = 18;
+        int targetTahunKuliah = 4;
+        double ipk = 3.89764512;
+        char nilaiAbjad = 'A';
+        boolean tampan = true;
+
+        System.out.println(namaDepan + namaTengah + namaBelakang);
+}
+}
